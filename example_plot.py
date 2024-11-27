e7ff085e9bb877cd9b365cc7cee3b3291c2da5c8
Max Generowicz <git-mdcg@my-post-office.net>
2024-11-27 13:38:11 +0000

    Add point-line (-o-o-o-) plot to fake data

--- example_plot.py
+++ example_plot.py
@@ -2,14 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Data
-xs = np.linspace(0, 10, 1000)
-ys1 = 2*np.sin(5*xs+3) + 0.2*np.cos(13*xs-1) + 8*np.sin(1.3*xs+5) + 0.4*np.sin(52*xs+3)
-ys2 = 3*np.sin(7*xs+14) + 0.3*np.cos(11*xs) + 6*np.sin(1.7*xs+1) + 0.3*np.cos(63*xs-9)
+wave_xs = np.linspace(0, 10, 1000)
+wave_ys1 = 2*np.sin(5*wave_xs+3) + 0.2*np.cos(13*wave_xs-1) + 8*np.sin(1.3*wave_xs+5) + 0.4*np.sin(52*wave_xs+3)
+wave_ys2 = 3*np.sin(7*wave_xs+14) + 0.3*np.cos(11*wave_xs) + 6*np.sin(1.7*wave_xs+1) + 0.3*np.cos(63*wave_xs-9)
+point_xs = np.linspace(0, 10, 20)
+point_ys = 5*np.sin(0.9*point_xs+23) + np.random.normal(scale=1.5, size=len(point_xs))
 
 # Plot
-plt.plot(xs, ys1, zorder=2, label="run 1")
-plt.plot(xs, ys2, zorder=2, label="run 2")
-plt.plot([min(xs), max(xs)], [0.5*max(ys1)]*2, linestyle='dashed', zorder=1, label="cutoff")
+plt.plot(wave_xs, wave_ys1, zorder=2, label="run 1")
+plt.plot(wave_xs, wave_ys2, zorder=2, label="run 2")
+plt.plot([min(wave_xs), max(wave_xs)], [0.5*max(wave_ys1)]*2, linestyle='dashed', zorder=1, label="cutoff")
+plt.plot(point_xs, point_ys, 'o-', label="theory")
 plt.grid()
 
 # Decorate
