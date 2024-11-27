9835efe4e885d472d61d7cf1449a3aa16ee42e3b
Max Generowicz <git-mdcg@my-post-office.net>
2024-11-27 13:45:53 +0000

    Add custom style file

--- example_plot.py
+++ example_plot.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.style.use('maxplotlib.mplstyle')
+
 # Data
+rng = np.random.default_rng(seed=0)
+
 wave_xs = np.linspace(0, 10, 1000)
 wave_ys1 = 2*np.sin(5*wave_xs+3) + 0.2*np.cos(13*wave_xs-1) + 8*np.sin(1.3*wave_xs+5) + 0.4*np.sin(52*wave_xs+3)
 wave_ys2 = 3*np.sin(7*wave_xs+14) + 0.3*np.cos(11*wave_xs) + 6*np.sin(1.7*wave_xs+1) + 0.3*np.cos(63*wave_xs-9)
 point_xs = np.linspace(0, 10, 20)
-point_ys = 5*np.sin(0.9*point_xs+23) + np.random.normal(scale=1.5, size=len(point_xs))
+point_ys = 5*np.sin(0.9*point_xs+22) + rng.normal(scale=1.5, size=len(point_xs))
 
 # Plot
 plt.plot(wave_xs, wave_ys1, zorder=2, label="run 1")
